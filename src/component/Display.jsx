846d11d21f634c5c240d10d2271a6a25a994aa71
Bhudeo Krit <Bhudeokrit@gmail.com>
2024-05-01 09:56:15 +0530

    improved

--- src/component/Display.jsx
+++ src/component/Display.jsx
@@ -1,16 +1,29 @@
-import React, { useState } from 'react'
-import image from "./images"
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import styles from "./Display.module.css"
 
 export default function Display() {
-    const [ind,setInd]=useState(Math.floor(Math.random()*image.length))
+    const [img,setImg]=useState()
+    const [ind,setInd]=useState(0)
+    useEffect(()=>{
+        async function handleChange(){
+            try{
+                const res=await axios.get("https://dog.ceo/api/breeds/image/random");
+                setImg(res.data.message);
+            }
+            catch(err){
+                console.log(err)
+            }
+        }
+        handleChange();
+    },[ind])
     return (
         <div className={styles.bigbox}>
             <div>
-            <img src={image[ind]} className={styles.imgs} alt={`img${ind}`}/>
+            <img src={img} className={styles.imgs} alt='img'/>
             </div>
             <div>
-                <button className={styles.btn} onClick={()=>setInd(Math.floor(Math.random()*image.length))}>Change Image</button>
+                <button className={styles.btn} onClick={()=>setInd(ind+1)}>Change Manas</button>
             </div>
         </div>
 
